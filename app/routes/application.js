d549c2ade3cddc7cf81686ad826292ca4bc46688
Matthew Sumner <matt.m.sumner@gmail.com>
2015-02-10 17:30:50 -0500

    currentUser and fetching sessions
    This sets the currentUser on open and fetch for application controller. Also,
    the app will attempt to fetch an existing session upon activating the
    application route. This should only happen once after the app has booted.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,14 +1,18 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  activate: function() {
+    this.get("session").fetch();
+  },
+
   actions: {
     signInViaGithub: function() {
       var route = this;
 
-      this.get('session').open('github-oauth2').then(function(){
-        alert('success!');
+      this.get("session").open("github-oauth2").then(function(authorization){
+        // do the things after login, like redirect to dashboard
       }, function(error) {
-        route.controller.set('error', 'Could not sign you in: ' + error.message);
+        route.controller.set("error", "Could not sign you in: " + error.message);
       });
     }
   }
--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,19 +1,33 @@
-import Ember from 'ember';
-import config from '../config/environment';
+import Ember from "ember";
+import config from "../config/environment";
 
 export default Ember.Object.extend({
   open: function(authorization){
-    var temporaryCode = authorization.authorizationCode;
+    return this._fetchSession({
+      "github-auth-code": authorization.authorizationCode
+    });
+  },
 
+  fetch: function() {
+    return this._fetchSession({
+      "token": localStorage.getItem("token")
+    });
+  },
+
+  _fetchSession: function(tokenData) {
     return new Ember.RSVP.Promise(function(resolve, reject){
       Ember.$.ajax({
         url: config.jibber.sessionUrl,
         type: "POST",
-        data: { 'github-auth-code': temporaryCode },
-        dataType: 'json',
-        success: Ember.run.bind(null, resolve),
+        data: tokenData,
+        dataType: "json",
+        success: function(data) {
+          localStorage.setItem("token", data.token);
+          var currentUser = Ember.Object.create(data);
+          Ember.run.bind(null, resolve({ "currentUser": currentUser }));
+        },
         error: function(jqXHR, textStatus, errorThrown){
-          Ember.run.bind(null, reject({ 'message': errorThrown }));
+          Ember.run.bind(null, reject({ "message": errorThrown }));
         }
       });
     });
